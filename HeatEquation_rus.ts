4edb0ef8c096d9c19fe5e0a6ee259c4bb81aab94
optfil <dergachev88@gmail.com>
2018-03-07 17:14:36 +0300

    Rus translations added.

---
+++ HeatEquation_rus.ts
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="form.cpp" line="108"/>
+        <source>Initial profile</source>
+        <translation>Начальный профиль импульса</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="129"/>
+        <source>Temperature profile</source>
+        <translation>Профиль температуры</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="131"/>
+        <source>Gauss</source>
+        <translation>Гаусс</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="132"/>
+        <source>SuperGauss</source>
+        <translation>Супергаусс</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="133"/>
+        <source>Rectangle</source>
+        <translation>Прямоугольник</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="134"/>
+        <source>Delta</source>
+        <translation>Дельта</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="136"/>
+        <source>Grid size</source>
+        <translation>Размер сетки</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="137"/>
+        <source> L = </source>
+        <translation> L = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="139"/>
+        <source>Integration time</source>
+        <translation>Время интегрирования</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="140"/>
+        <source> T = </source>
+        <translation> T = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="142"/>
+        <source>Number of spatial points</source>
+        <translation>Количество точек по пространству</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="143"/>
+        <source>NX = </source>
+        <translation>NX = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="145"/>
+        <source>Number of temporal points</source>
+        <translation>Количество точек по времени</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="146"/>
+        <source>NT = </source>
+        <translation>NT = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="178"/>
+        <source>Spatial step</source>
+        <translation>Шаг по пространству</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="179"/>
+        <source>dx = </source>
+        <translation>dx = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="183"/>
+        <source>Time step</source>
+        <translation>Шаг по времени</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="184"/>
+        <source>dt = </source>
+        <translation>dt = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="188"/>
+        <source>Alpha</source>
+        <translation>Альфа</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="189"/>
+        <source>α = </source>
+        <translation>α = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="193"/>
+        <source>Start</source>
+        <translation>Старт</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="201"/>
+        <location filename="form.cpp" line="330"/>
+        <location filename="form.cpp" line="459"/>
+        <source>Dispersion</source>
+        <translation>Дисперсия</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="239"/>
+        <location filename="form.cpp" line="368"/>
+        <location filename="form.cpp" line="497"/>
+        <source>Dissipation</source>
+        <translation>Диссипация</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="274"/>
+        <location filename="form.cpp" line="403"/>
+        <location filename="form.cpp" line="532"/>
+        <source>Solution</source>
+        <translation>Решение</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="294"/>
+        <location filename="form.cpp" line="423"/>
+        <location filename="form.cpp" line="552"/>
+        <source>Error</source>
+        <translation>Погрешность</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="583"/>
+        <source>Explicit</source>
+        <translation>Явная схема</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="584"/>
+        <source>Implicit</source>
+        <translation>Неявная схема</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="585"/>
+        <source>Crank-Nicolson</source>
+        <translation>Схема Кранка-Николсон</translation>
+    </message>
+</context>
+</TS>
